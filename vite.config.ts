c72941f88ea99c116e11697fa4b2e0ba219b4ba9
a-nakashima-seta <a-nakashima@s-shizensyokuhin.co.jp>
2024-10-24 13:41:03 +0900

    viteconfig 修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/bundle.js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  base: './'
 })
