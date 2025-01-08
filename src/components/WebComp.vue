7b5a248d2c7a9a608de8da3bd9dc7991a51e801f
a-nakashima-seta <a-nakashima@s-shizensyokuhin.co.jp>
2025-01-08 16:18:29 +0900

    web版URLのinputをshadcnに

--- src/components/WebComp.vue
+++ src/components/WebComp.vue
@@ -1,92 +1,109 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 import { CheckItem } from "../types/types";
+import { Input } from "../components/ui/input";
 
 import {
-    checkPageTitle,
-    checkWebPreheader,
-    checkWebApplicationNo,
-    checkImageLinks,
-    checkUTMCampaign,
-    checkWebCPNLinkText,
-    checkWebOpenTag,
-    checkNoIndexMetaTag,
-    checkWebFooter,
-    checkGTM,
-    checkFavicon
+  checkPageTitle,
+  checkWebPreheader,
+  checkWebApplicationNo,
+  checkImageLinks,
+  checkUTMCampaign,
+  checkWebCPNLinkText,
+  checkWebOpenTag,
+  checkNoIndexMetaTag,
+  checkWebFooter,
+  checkGTM,
+  checkFavicon,
 } from "../utils/CheckFunc";
-import WebModeToggle from './WebModeToggle.vue';
+import WebModeToggle from "./WebModeToggle.vue";
 
 const WebCheckList = ref<CheckItem[]>([
-    { id: "mail1", label: "タイトルは正しいか", checkFn: checkPageTitle },
-    { id: "web2", label: "プリヘッダーはないか", checkFn: checkWebPreheader },
-    { id: "web3", label: "冒頭に変数はないか", checkFn: checkWebApplicationNo },
-    { id: "web4", label: "画像のリンク切れはないか", checkFn: checkImageLinks },
-    { id: "web5", label: "$$$utm_campaign$$$がないか", checkFn: checkUTMCampaign },
-    { id: "web6", label: "※画像がうまく表示されない方はこちらがないか", checkFn: checkWebCPNLinkText },
-    { id: "web7", label: "開封タグはないか", checkFn: checkWebOpenTag },
-    { id: "web8", label: "noindexの記述はあるか", checkFn: checkNoIndexMetaTag },
-    { id: "web9", label: "フッターが変数化されていないか", checkFn: checkWebFooter },
-    { id: "web10", label: "GTM用の記述があるか", checkFn: checkGTM },
-    { id: "web11", label: "faviconは設定されているか", checkFn: checkFavicon }
+  { id: "mail1", label: "タイトルは正しいか", checkFn: checkPageTitle },
+  { id: "web2", label: "プリヘッダーはないか", checkFn: checkWebPreheader },
+  { id: "web3", label: "冒頭に変数はないか", checkFn: checkWebApplicationNo },
+  { id: "web4", label: "画像のリンク切れはないか", checkFn: checkImageLinks },
+  {
+    id: "web5",
+    label: "$$$utm_campaign$$$がないか",
+    checkFn: checkUTMCampaign,
+  },
+  {
+    id: "web6",
+    label: "※画像がうまく表示されない方はこちらがないか",
+    checkFn: checkWebCPNLinkText,
+  },
+  { id: "web7", label: "開封タグはないか", checkFn: checkWebOpenTag },
+  { id: "web8", label: "noindexの記述はあるか", checkFn: checkNoIndexMetaTag },
+  {
+    id: "web9",
+    label: "フッターが変数化されていないか",
+    checkFn: checkWebFooter,
+  },
+  { id: "web10", label: "GTM用の記述があるか", checkFn: checkGTM },
+  { id: "web11", label: "faviconは設定されているか", checkFn: checkFavicon },
 ]);
 
 let WebSource: string = "";
 const selectedChecksWeb = ref(new Array(WebCheckList.value.length).fill(true));
 const errorMessages = ref<string[]>([]);
-const statusResults = ref<string[]>(new Array(WebCheckList.value.length).fill(''));
+const statusResults = ref<string[]>(
+  new Array(WebCheckList.value.length).fill("")
+);
 const checklistRef = ref<HTMLElement | null>(null);
-const url = ref<string>('');
-const checkTypeWeb = ref<string>("通常")
+const url = ref<string>("");
+const checkTypeWeb = ref<string>("通常");
 
 const selectAll = () => {
-    selectedChecksWeb.value.fill(true);
-}
+  selectedChecksWeb.value.fill(true);
+};
 
 const clearSelections = () => {
-    selectedChecksWeb.value.fill(false);
+  selectedChecksWeb.value.fill(false);
 };
 
 const getWebSource = async () => {
-    if (!url.value) {
-        alert('URLが入力されていません。');
-        return;
+  if (!url.value) {
+    alert("URLが入力されていません。");
+    return;
+  }
+
+  try {
+    const username = "Setagaya";
+    const password = "setagaya1234";
+    const encodedString = btoa(`${username}:${password}`);
+    const encodedAuth = `Basic ${encodedString}`;
+
+    const response = await fetch(
+      "http://www2.shizensyokuhin.jp/tool/html-checker/getSource.php",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: encodedAuth,
+        },
+        body: JSON.stringify({ url: url.value }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("ネットワークエラー: " + response.status);
     }
 
-    try {
-        const username = "Setagaya"
-        const password = "setagaya1234"
-        const encodedString = btoa(`${username}:${password}`);
-        const encodedAuth = `Basic ${encodedString}`;
+    const res = await response.json();
 
-        const response = await fetch('http://www2.shizensyokuhin.jp/tool/html-checker/getSource.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": encodedAuth,
-            },
-            body: JSON.stringify({ url: url.value })
-        });
-
-        if (!response.ok) {
-            throw new Error('ネットワークエラー: ' + response.status);
-        }
-
-        const res = await response.json();
-
-        if (res.html) {
-            WebSource = res.html
-        }
-
-        return true
-    } catch (error) {
-        alert('ページソースの取得に失敗しました');
-        console.error('ページソースの取得に失敗しました:', error);
-
-        return false
+    if (res.html) {
+      WebSource = res.html;
     }
+
+    return true;
+  } catch (error) {
+    alert("ページソースの取得に失敗しました");
+    console.error("ページソースの取得に失敗しました:", error);
+
+    return false;
+  }
 };
-
 
 // キャプチャは手動で保存する運用のためいったんコメントアウト
 // const captureChecklist = async () => {
@@ -105,146 +122,192 @@ const getWebSource = async () => {
 // };
 
 const checkWebSource = async () => {
-    if (selectedChecksWeb.value.every((checked) => !checked)) {
-        alert("チェック項目を選択してください");
-        return;
-    }
+  if (selectedChecksWeb.value.every((checked) => !checked)) {
+    alert("チェック項目を選択してください");
+    return;
+  }
 
+  await getWebSource();
 
-    await getWebSource()
+  if (WebSource !== "") {
+    errorMessages.value = [];
+    statusResults.value.fill("");
 
-    if (WebSource !== "") {
-        errorMessages.value = [];
-        statusResults.value.fill('');
+    const runAllChecks = async () => {
+      for (let i = 0; i < WebCheckList.value.length; i++) {
+        if (selectedChecksWeb.value[i]) {
+          const { checkFn } = WebCheckList.value[i];
+          const result = await checkFn(WebSource);
 
-        const runAllChecks = async () => {
-
-            for (let i = 0; i < WebCheckList.value.length; i++) {
-                if (selectedChecksWeb.value[i]) {
-                    const { checkFn } = WebCheckList.value[i];
-                    const result = await checkFn(WebSource);
-
-                    if (Array.isArray(result)) {
-                        result.forEach((message) => {
-                            if (message) {
-                                errorMessages.value.push(message);
-                            }
-                        });
-                    } else {
-                        if (result) {
-                            errorMessages.value.push(result);
-                        }
-                    }
-                    statusResults.value[i] = result && result.length > 0 ? 'NG' : 'OK';
-                }
+          if (Array.isArray(result)) {
+            result.forEach((message) => {
+              if (message) {
+                errorMessages.value.push(message);
+              }
+            });
+          } else {
+            if (result) {
+              errorMessages.value.push(result);
             }
-            if (selectedChecksWeb.value[10]) {
-                const faviconCheck = checkFavicon(WebSource, checkTypeWeb.value === "SEAC");
-
-                if (faviconCheck) {
-                    errorMessages.value.push(faviconCheck);
-                }
-                statusResults.value[10] = faviconCheck ? 'NG' : 'OK';
-            }
-        };
-
-
-        await runAllChecks()
-
-        const isSuccess = errorMessages.value.every(value => value == "");
-        if (isSuccess) {
-            alert("チェックOKです！")
-            // キャプチャは手動で保存する運用のためいったんコメントアウト
-            // await captureChecklist();
-        } else {
-            alert("エラー項目を確認して下さい。")
+          }
+          statusResults.value[i] = result && result.length > 0 ? "NG" : "OK";
         }
+      }
+      if (selectedChecksWeb.value[10]) {
+        const faviconCheck = checkFavicon(
+          WebSource,
+          checkTypeWeb.value === "SEAC"
+        );
 
+        if (faviconCheck) {
+          errorMessages.value.push(faviconCheck);
+        }
+        statusResults.value[10] = faviconCheck ? "NG" : "OK";
+      }
+    };
+
+    await runAllChecks();
+
+    const isSuccess = errorMessages.value.every((value) => value == "");
+    if (isSuccess) {
+      alert("チェックOKです！");
+      // キャプチャは手動で保存する運用のためいったんコメントアウト
+      // await captureChecklist();
+    } else {
+      alert("エラー項目を確認して下さい。");
     }
-
+  }
 };
 </script>
 
 <template>
-    <div style="width: 100%; max-width: 800px; margin: 0 auto;">
-        <h2>Web用チェックリスト</h2>
-        <div class="webInputArea">
-            <!-- <div class="webCheckType">
+  <div style="width: 100%; max-width: 800px; margin: 0 auto">
+    <h2>Web用チェックリスト</h2>
+    <div class="webInputArea">
+      <!-- <div class="webCheckType">
                 <input type="radio" id="normal" name="checkType" value="normal" v-model="checkTypeWeb">
                 <label for="normal">通常</label>
                 <input type="radio" id="seac" name="checkType" value="seac" v-model="checkTypeWeb">
                 <label for="seac">SEAC</label>
             </div> -->
 
-            <WebModeToggle v-model="checkTypeWeb" />
+      <WebModeToggle v-model="checkTypeWeb" />
 
-            <input class="getWebSourceArea" type="text" placeholder="チェック対象のurlを入力してください。" v-model="url">
-        </div>
+      <!-- <input
+        class="getWebSourceArea"
+        type="text"
+        placeholder="チェック対象のurlを入力してください。"
+        v-model="url"
+      /> -->
 
-        <div class="wrapper">
-            <div class="selectButtonWrapper">
-                <button class="selectButton" @click="selectAll">全選択</button>
-                <button class="selectButton" @click="clearSelections">選択解除</button>
-            </div>
-
-            <ul class="checkList" ref="checklistRef">
-                <h3 class="checkTypeName">Web</h3>
-                <li v-for="(item, index) in WebCheckList" :key="item.id">
-                    <input type="checkbox" :id="item.id" v-model="selectedChecksWeb[index]">
-                    <label :for="item.id">{{ item.label }}</label>
-                    <span :style="{ color: statusResults[index] === 'NG' ? '#f15f5f' : '#3FB27F' }">{{ statusResults[index] }}</span>
-                </li>
-            </ul>
-            <button class="checkButton" @click="checkWebSource">チェック実行</button>
-
-
-            <div class="errListWrapper" v-if="errorMessages.length && errorMessages.some(msg => msg.trim() !== '')">
-                <h2>エラーリスト</h2>
-                <ul class="errList">
-                    <li class="errListItem" v-for="(message, index) in errorMessages" :key="index">
-                        <p>
-                            {{ message }}
-                        </p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
+      <Input
+        type="text"
+        placeholder="チェック対象のurlを入力してください。"
+        v-model="url"
+      />
     </div>
+
+    <div class="wrapper">
+      <div class="selectButtonWrapper">
+        <button
+          class="selectButton"
+          @click="selectAll"
+        >
+          全選択
+        </button>
+        <button
+          class="selectButton"
+          @click="clearSelections"
+        >
+          選択解除
+        </button>
+      </div>
+
+      <ul
+        class="checkList"
+        ref="checklistRef"
+      >
+        <h3 class="checkTypeName">Web</h3>
+        <li
+          v-for="(item, index) in WebCheckList"
+          :key="item.id"
+        >
+          <input
+            type="checkbox"
+            :id="item.id"
+            v-model="selectedChecksWeb[index]"
+          />
+          <label :for="item.id">{{ item.label }}</label>
+          <span
+            :style="{
+              color: statusResults[index] === 'NG' ? '#f15f5f' : '#3FB27F',
+            }"
+            >{{ statusResults[index] }}</span
+          >
+        </li>
+      </ul>
+      <button
+        class="checkButton"
+        @click="checkWebSource"
+      >
+        チェック実行
+      </button>
+
+      <div
+        class="errListWrapper"
+        v-if="
+          errorMessages.length && errorMessages.some((msg) => msg.trim() !== '')
+        "
+      >
+        <h2>エラーリスト</h2>
+        <ul class="errList">
+          <li
+            class="errListItem"
+            v-for="(message, index) in errorMessages"
+            :key="index"
+          >
+            <p>
+              {{ message }}
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .getWebSourceArea {
-    display: block;
-    box-sizing: border-box;
-    margin: 0 auto;
-    font-size: 16px;
-    height: 20px;
-    max-width: 420px;
-    width: 100%;
-    margin-bottom: 1em;
-    border: none;
-    height: 30px;
-    border: solid 1px #213547;
-    border-radius: 5px;
+  display: block;
+  box-sizing: border-box;
+  margin: 0 auto;
+  font-size: 16px;
+  height: 20px;
+  max-width: 420px;
+  width: 100%;
+  margin-bottom: 1em;
+  border: none;
+  height: 30px;
+  border: solid 1px #213547;
+  border-radius: 5px;
 }
 
 .getWebSourceArea:focus-visible {
-    box-sizing: border-box;
-    border: solid 2px #00DC82;
-    outline: none;
+  box-sizing: border-box;
+  border: solid 2px #00dc82;
+  outline: none;
 }
 
 .webInputArea {
-    margin-top: 55px;
-    margin-bottom: 67px;
+  margin-top: 55px;
+  margin-bottom: 67px;
 }
 
 .webCheckType {
-    max-width: 420px;
-    display: flex;
-    justify-content: flex-end;
-    margin: 0 auto;
-    margin-bottom: .5em;
+  max-width: 420px;
+  display: flex;
+  justify-content: flex-end;
+  margin: 0 auto;
+  margin-bottom: 0.5em;
 }
 </style>
